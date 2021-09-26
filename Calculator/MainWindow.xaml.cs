a95e0c44bddc5097efac0879a506e830432a868c
AmirSina Mashayekh <pajuhesh80@outlook.com>
2021-09-26 22:10:15 +0330

    Made buttons functional

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -1,52 +1,95 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Calculator
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
 
-		private void ResetResultBox()
-		{
-			resultBox.Foreground = Brushes.Gray;
-			resultBox.Text = "Result";
-		}
+        private void ResetResultBox()
+        {
+            resultBox.Foreground = Brushes.Gray;
+            resultBox.Text = "Result";
+        }
 
-		private void InputBox_GotFocus(object sender, RoutedEventArgs e)
-		{
-			if (inputBox.Foreground == Brushes.Gray)
-			{
-				inputBox.Clear();
-				inputBox.Foreground = Brushes.White;
-			}
-		}
+        private void ResetInputBox()
+        {
+            inputBox.Foreground = Brushes.Gray;
+            inputBox.Text = "Input";
+        }
 
-		private void InputBox_LostFocus(object sender, RoutedEventArgs e)
-		{
-			if (string.IsNullOrWhiteSpace(inputBox.Text))
-			{
-				inputBox.Foreground = Brushes.Gray;
-				inputBox.Text = "Input";
-			}
-		}
-	}
+        private void InsertInput(string text, int caretIndexOffset = 0)
+        {
+            if (inputBox.Foreground == Brushes.Gray)
+            {
+                inputBox.Text = "";
+                inputBox.Foreground = Brushes.White;
+            }
+
+            int caretIndex = inputBox.CaretIndex;
+            inputBox.Text = inputBox.Text.Insert(inputBox.CaretIndex, text);
+            inputBox.CaretIndex = caretIndex + text.Length + caretIndexOffset;
+        }
+
+        private void InputBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (inputBox.Foreground == Brushes.Gray)
+            {
+                inputBox.Clear();
+                inputBox.Foreground = Brushes.White;
+            }
+        }
+
+        private void InputBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(inputBox.Text)) ResetInputBox();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            ResetResultBox();
+            ResetInputBox();
+        }
+
+        private void FunctionButtons_Click(object sender, RoutedEventArgs e)
+        {
+            string text = (e.Source as Button).Content.ToString() + "()";
+            InsertInput(text, -1);
+        }
+
+        private void SelfInsert_Click(object sender, RoutedEventArgs e)
+        {
+            string text = (e.Source as Button).Content.ToString();
+            InsertInput(text);
+        }
+
+        private void Power_Click(object sender, RoutedEventArgs e)
+        {
+            InsertInput("^");
+        }
+
+        private void Factorial_Click(object sender, RoutedEventArgs e)
+        {
+            InsertInput("!");
+        }
+
+        private void Absolute_Click(object sender, RoutedEventArgs e)
+        {
+            InsertInput("||", -1);
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            resultBox.Foreground = Brushes.White;
+            resultBox.Text = "Nothing yet...";
+        }
+    }
 }
