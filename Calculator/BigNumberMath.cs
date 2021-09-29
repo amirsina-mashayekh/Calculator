7a74bf3114e8813913da8b4d4a62fd6b17025967
AmirSina Mashayekh <pajuhesh80@outlook.com>
2021-09-29 11:46:29 +0330

    Added BigNumberMath class Implemented DivideWithDecimals, Factorial and Exponent

---
+++ Calculator/BigNumberMath.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Advanced arithmetic for BigNumbers.
+    /// </summary>
+    public class BigNumberMath
+    {
+        private static readonly BigNumber zero = new BigNumber("0");
+
+        private static readonly BigNumber one = new BigNumber("1");
+
+        private static readonly BigNumber ten = new BigNumber("10");
+
+        public static BigNumber DivideWithDecimals(BigNumber n, BigNumber n1, int decimalPoints = 10)
+        {
+            if (decimalPoints < 0)
+            {
+                throw new ArgumentOutOfRangeException("Decimal points should be at least 0.");
+            }
+
+            BigNumber divisioned = new BigNumber(n.Value);
+            BigNumber divisor = new BigNumber(n1.Value);
+
+            for (uint i = 0; i < decimalPoints; i++)
+            {
+                divisioned *= ten;
+            }
+
+            BigNumber result = divisioned / divisor;
+            string rstr = result.Abs().Value;
+
+            while (decimalPoints >= rstr.Length)
+            {
+                // Add 0s before number to be able to insert decimal point
+                rstr = '0' + rstr;
+            }
+
+            return new BigNumber((result.Sign ? "" : "-") + rstr.Insert(rstr.Length - decimalPoints, "."));
+        }
+
+        public static BigNumber Factorial(BigNumber n)
+        {
+            if (!n.Sign || n.DecimalPart != "")
+            {
+                throw new ArithmeticException("Factorial is only supported for zero and positive integers.");
+            }
+
+            BigNumber result = new BigNumber("1");
+
+            for (BigNumber i = new BigNumber("1"); i <= n; i++)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+
+        public static BigNumber Exponent(BigNumber n, BigNumber n1)
+        {
+            if (n1.DecimalPart != "")
+            {
+                throw new ArithmeticException("Decimal power is not supported yet.");
+            }
+
+            if (n1 == zero && n == n1)
+            {
+                throw new ArithmeticException("Zero raised to zero is undefined.");
+            }
+
+            BigNumber result = new BigNumber("1");
+
+            if (n1.Sign)
+            {
+                for (BigNumber i = new BigNumber("0"); i < n1; i++)
+                {
+                    result *= n;
+                }
+            }
+            else
+            {
+                result = DivideWithDecimals(one, Exponent(n, -n1));
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CalculatorTests/BigNumberMathTests.cs
@@ -0,0 +1,97 @@
+﻿using Calculator;
+using System;
+using static Calculator.BigNumberMath;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Tests
+{
+    [TestClass()]
+    public class BigNumberMathTests
+    {
+        [TestMethod(), Timeout(100)]
+        public void FactorialTest()
+        {
+            string[,] tests =
+            {
+                { "0", "1" },
+                { "1", "1" },
+                { "2", "2" },
+                { "4", "24" },
+                { "4", "24" },
+                { "20", "2432902008176640000" },
+                { "29", "8841761993739701954543616000000" }
+            };
+
+            for (int i = 0; i < tests.GetLength(0); i++)
+            {
+                BigNumber n = new BigNumber(tests[i, 0]);
+                BigNumber res = new BigNumber(tests[i, 1]);
+                BigNumber fact = Factorial(n);
+                Debug.Print("res: " + res + "    fact:" + fact);
+                Assert.IsTrue(fact == res);
+            }
+
+            _ = Assert.ThrowsException<ArithmeticException>(() => Factorial(new BigNumber("-1")));
+            _ = Assert.ThrowsException<ArithmeticException>(() => Factorial(new BigNumber("1.1")));
+            _ = Assert.ThrowsException<ArithmeticException>(() => Factorial(new BigNumber("-1.1")));
+        }
+
+        [TestMethod(), Timeout(100)]
+        public void ExponentTest()
+        {
+            string[,] tests =
+            {
+                { "0", "1", "0" },
+                { "1", "100", "1" },
+                { "2", "2", "4" },
+                { "9", "9", "387420489" },
+                { "-2", "2", "4" },
+                { "-2", "0", "1" },
+                { "3.3", "3", "35.937" },
+                { "2", "-2", "0.25" },
+                { "-3", "-3", "-0.0370370370" }
+            };
+
+            for (int i = 0; i < tests.GetLength(0); i++)
+            {
+                BigNumber n = new BigNumber(tests[i, 0]);
+                BigNumber n1 = new BigNumber(tests[i, 1]);
+                BigNumber res = new BigNumber(tests[i, 2]);
+                BigNumber pow = Exponent(n, n1);
+                Debug.Print("res: " + res + "    pow:" + pow);
+                Assert.IsTrue(pow == res);
+            }
+
+            BigNumber zero = new BigNumber("0");
+            _ = Assert.ThrowsException<ArithmeticException>(() => Exponent(zero, zero));
+            _ = Assert.ThrowsException<ArithmeticException>(() => Exponent(new BigNumber("1"), new BigNumber("1.1")));
+        }
+
+        [TestMethod(), Timeout(100)]
+        public void DivideWithDecimalsTest()
+        {
+            string[,] tests =
+            {
+                { "0", "1", "5", "0" },
+                { "1", "10", "10", "0.1" },
+                { "2", "3", "10", "0.6666666666" },
+                { "5", "2", "10", "2.5" },
+                { "2299", "66", "20", "34.83333333333333333333" }
+            };
+
+            for (int i = 0; i < tests.GetLength(0); i++)
+            {
+                BigNumber n = new BigNumber(tests[i, 0]);
+                BigNumber n1 = new BigNumber(tests[i, 1]);
+                BigNumber res = new BigNumber(tests[i, 3]);
+                BigNumber div = DivideWithDecimals(n, n1, int.Parse(tests[i, 2]));
+                Debug.Print("res: " + res + "    div:" + div);
+                Assert.IsTrue(div == res);
+            }
+
+            BigNumber zero = new BigNumber("0");
+            _ = Assert.ThrowsException<ArithmeticException>(() => DivideWithDecimals(zero, zero));
+        }
+    }
+}
