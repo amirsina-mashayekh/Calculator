ce447f9cc85d59c3c2effc5798b195ed3c43fa57
AmirSina Mashayekh <pajuhesh80@outlook.com>
2021-10-02 12:23:23 +0330

    Added Evaluator class Added EvaluateRPN method

---
+++ Calculator/Evaluator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using BigNumbers;
+using static BigNumbers.BigNumberMath;
+
+namespace Calculator
+{
+    public static class Evaluator
+    {
+        /// <summary>
+        /// Evaluates a Reverse Polish notation expression.
+        /// </summary>
+        /// <param name="rpn">An array of operators and <c>BigNumber</c>s in RPN order.</param>
+        /// <returns>A <c>BigNumber</c> showing the result of expression.</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static BigNumber EvaluateRPN(object[] rpn)
+        {
+            Stack<BigNumber> stack = new Stack<BigNumber>();
+
+            foreach (object token in rpn)
+            {
+                if (token is BigNumber)
+                {
+                    stack.Push(token as BigNumber);
+                }
+                else if (token is string)
+                {
+                    stack.Push(Operate(token as string, stack));
+                }
+                else
+                {
+                    throw new ArgumentException("Invalid token in RPN expression.");
+                }
+            }
+
+            return stack.Count == 1 ? stack.Pop() : throw new ArgumentException("Invalid expression.");
+        }
+
+        /// <summary>
+        /// Applies the operator on last <c>BigNumber</c>(s) in a stack.
+        /// </summary>
+        /// <param name="Operator">The operator to be applied.</param>
+        /// <param name="stack">The stack of <c>BigNumber</c>s.</param>
+        /// <returns>A <c>BigNumber</c> showing the result of operation.</returns>
+        private static BigNumber Operate(string Operator, Stack<BigNumber> stack)
+        {
+            BigNumber num = stack.Pop();
+
+            return Operator switch
+            {
+                "+" => stack.Pop() + num,
+                "-" => stack.Pop() - num,
+                "*" => stack.Pop() * num,
+                "/" => DivideWithDecimals(stack.Pop(), num),
+                "%" => stack.Pop() % num,
+                "^" => Exponent(stack.Pop(), num),
+                "|" => num.Abs(),
+                "!" => Factorial(num),
+                "sin" => Sinus(num),
+                "cos" => Cosinus(num),
+                "tan" => Tangent(num),
+                "cot" => Cotangent(num),
+                _ => throw new ArgumentException("Invalid operator: " + Operator),
+            };
+        }
+    }
+}
---
+++ CalculatorTests/EvaluatorTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static Calculator.Evaluator;
+using BigNumbers;
+using static BigNumbers.BigNumberMath;
+
+namespace Calculator.Tests
+{
+    [TestClass()]
+    public class EvaluatorTests
+    {
+        [TestMethod()]
+        public void EvaluateRPNTest()
+        {
+            // 5.3 * (2 + |sin(-pi/2) + cos(pi/4)|) - (tan(0) + cot(pi/3)) + 4! ^ 3 % 7
+            // == 17.5759
+            BigNumber _5_3 = new BigNumber(5.3M);
+            BigNumber _2 = new BigNumber(2M);
+            BigNumber pi = new BigNumber((decimal)Math.PI);
+            BigNumber _3 = new BigNumber(3M);
+            BigNumber _4 = new BigNumber(4M);
+            BigNumber _0 = new BigNumber(0M);
+            BigNumber _7 = new BigNumber(7M);
+
+            object[] test =
+            {
+                _5_3, _2, -pi, _2, "/", "sin", pi, _4, "/", "cos", "+", "|", "+", "*",
+                _0, "tan", pi, _3, "/", "cot", "+", "-", _4, "!", _3, "^", _7, "%", "+"
+            };
+
+            BigNumber result =
+                (_5_3 * (_2 + (Sinus(DivideWithDecimals(-pi, _2)) + Cosinus(DivideWithDecimals(pi, _4))).Abs()))
+                - (Tangent(_0) + Cotangent(DivideWithDecimals(pi, _3))) + (Exponent(Factorial(_4), _3) % _7);
+
+            Assert.AreEqual(result, EvaluateRPN(test));
+
+            test[^1] = 2;
+            _ = Assert.ThrowsException<ArgumentException>(() => EvaluateRPN(test));
+            test[^1] = "Hello!";
+            _ = Assert.ThrowsException<ArgumentException>(() => EvaluateRPN(test));
+            test[^1] = _7;
+            _ = Assert.ThrowsException<ArgumentException>(() => EvaluateRPN(test));
+        }
+    }
+}
